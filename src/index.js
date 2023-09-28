e391f3cf49e5d72c5a4855908134b0098496302f
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-28 13:47:05 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const taskForm = document.getElementById("create-task-form");
+
+
+  taskForm.addEventListener('submit',  (e) => {
+    e.preventDefault(); // Prevent form submission
+    createToDo(taskForm.querySelector('#new-task-description').value)
+    taskForm.reset()
+
+
+    function createToDo(toDo) {
+      let listItem = document.createElement('li')
+      let btn = document.createElement('button')
+      listItem.textContent=  toDo ;
+      btn.textContent =' X '
+      btn.addEventListener('click', handleDelete)
+      
+      document.querySelector('#tasks').appendChild(listItem)
+      listItem.appendChild(btn)
+    }
+
+    function handleDelete(e) {
+      e.target.parentNode.remove()
+    }
+  });
+})
